8395baf9b0cb6320c02b89cd0ace5c9d733ba26b
Ashik Varma <varmaashik@gmail.com>
2020-03-17 23:11:32 +0530

    second commit

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const navbarTransition = ()=>{
 const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".navLinks");
-const navLinkList = document.querySelectorAll(".navLinks ul li");
+const navLinks = document.querySelector(".nav-links");
+const navLinkList = document.querySelectorAll(".nav-links  li");
 // const loading = document.querySelectorAll('#loading path');
 
 // for (let i=0;i<loading.length;i++){
 
 // }
-console.log(navLinkList)
+
+console.log(navLinks,navLinkList)
+
 hamburger.addEventListener("click",function(){
 // Burger Animation
 
-hamburger.classList.toggle('toggle');
+// hamburger.classList.toggle('toggle');
 navLinks.classList.toggle('showNav');
 navLinkList.forEach((link,index)=>{
     if(link.style.animation){
